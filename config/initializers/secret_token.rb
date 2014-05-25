7b289124896a22e38c7fada90a9fe7232075b9b3
Yeed <yoktars@gmail.com>
2014-05-25 13:45:49 +0300

    v1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jamdancedaddymovemomma::Application.config.secret_token = '7c459923d09eb8374eefa22e26bd5c9375eb4e9f5d6b5186f9d5f3550d05e9078841f7df9f96e4ff89f16e14689390b833b427792cc6a2985e8b039c14698d77'
