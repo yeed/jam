61eca8b2edca0c7dd2d50c293ae0819dac2d6e40
Yeed <yoktars@gmail.com>
2014-07-10 00:19:42 +0300

    cross browser

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -54,69 +54,7 @@ class HomeController < ApplicationController
     end
 
 	def initvideos
-		require "net/https"
-		require "uri"
-		require 'json'
-
 		
-
-		uri = URI.parse("https://api.instagram.com/v1/tags/Jam25kentry/media/recent?access_token=201508105.1fb234f.23f267ecdf4d409980c2d4163a3e3b4e&max_tag_id=1402827536318607")
-		http = Net::HTTP.new(uri.host, uri.port)
-		http.use_ssl = true
-		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-		request = Net::HTTP::Get.new(uri.request_uri)
-
-		response = http.request(request)
-		puts response.body
-
-		hash = JSON.parse response.body
-		
-		@text = ""
-
-
-		hash['data'].each do |child|
-
-			if child['type'] == "video"
-				if Video.exists?(:video_id => child['id'])
-					@text << "Video Kaydedilmis :(\n\n"
-				else
-					unless child['caption'].nil?
-
-						@text << "Video Kaydedilecek!\n\n"
-
-						@videoid = child['id']
-						@videoname = child['caption']['text']
-						@videourl = child['videos']['standard_resolution']['url']
-						@imageurl = child['images']['standard_resolution']['url']
-						@username = child['user']['username']
-						@shareurl = child['link']
-
-						@text << 'Video Ismi: ' << @videoname << "\n"
-						@text << 'Video Url: ' << @videourl << "\n"
-						@text << 'Video Image Url: ' << @imageurl << "\n"
-						@text << 'Video ID: ' << @videoid << "\n"
-						@text << 'User Name: ' << @username << "\n"
-						@text << 'Share url: ' << @shareurl << "\n"
-		  	
-						@video = Video.new
-
-						@video.name = @username
-						@video.description = @videoname
-						@video.url = @videourl
-						@video.thumbnail_image_url = @imageurl
-						@video.video_id = @videoid
-						@video.view_count = 0
-						@video.shareurl = @shareurl
-
-						@video.save
-					else
-						@text << "Video has null fields!\n\n"
-				  		next
-					end
-				end
-			end
-		end
 	end
 
 	def initvinevideos
