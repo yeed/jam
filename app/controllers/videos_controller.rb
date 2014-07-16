06bf2a39de4ef957dce34bc4e134902a5a78a62c
Yeed <yoktars@gmail.com>
2014-07-17 02:44:24 +0300

    remove sticky footer

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -31,37 +31,4 @@ class VideosController < ApplicationController
 		@searchparam = params['search']
 	end
 
-	def stickvideos
-		@leftsidebgcolor = "orange"
-		@rightsidebgcolor = "pink"
-		@canvascolor = "#db438f"
-
-		if request.original_url.include? "pop"
-			videos = Video.order("videos.view_count DESC")
-		else
-			videos = Video.order("videos.created_at DESC")
-		end
-
-		videos = videos.where(ignored: nil)
-
-		@searchmode = false
-
-		if params['search'] != nil
-			videos = videos.where(name: params['search'])
-			
-			
-			@searchmode = true
-
-			if videos.count > 0
-				@rightsidebgcolor = "darkblue"
-				@canvascolor = "#3072c4"
-			end
-
-		end
-		
-		@videos = videos
-		@searchparam = params['search']
-	end
-
-
 end
